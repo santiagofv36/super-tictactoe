1909bab025f843d4ce0d87cb29bf129b0a2a7126
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-05-25 00:29:58 -0400

    refactor: 🧹 adding dynamic depth & plays array

--- src/components/Tictactoe.tsx
+++ src/components/Tictactoe.tsx
@@ -1,9 +1,16 @@
+import { Play } from '../types/enums';
 import Grid from './Grid';
 
-export default function Tictactoe() {
+interface TictactoeProps {
+  depth: number;
+  plays: Play[];
+  setPlays: React.Dispatch<React.SetStateAction<Play[]>>;
+}
+
+export default function Tictactoe({ depth, plays, setPlays }: TictactoeProps) {
   return (
     <div className="h-[800px] flex justify-center items-center">
-      <Grid depth={2} limit={3} />
+      <Grid depth={depth} limit={3} plays={plays} setPlays={setPlays} />
     </div>
   );
 }
